d4262b507adf77b32b23cd1f6e05ba9415a0e419
Bracken Mosbacker <bracken@lumenlearning.com>
2016-10-23 19:33:32 -0700

    add licensing helper

---
+++ lib/license_helper.rb
@@ -0,0 +1,11 @@
+module LicenseHelper
+  LICENSES = {public_domain: 'No Rights Reserved (Public Domain)',
+              cc_by: 'CC BY (Attribution)',
+              cc_by_sa: 'CC BY-SA (Attribution ShareAlike)',
+              cc_by_nd: 'CC BY-ND (Attribution NoDerivatives)',
+              cc_by_nc: 'CC BY-NC (Attribution NonCommercial)',
+              cc_by_nc_sa: 'CC BY-NC-SA (Attribution NonCommercial ShareAlike)',
+              cc_by_nc_nd: 'CC BY-NC-ND (Attribution NonCommercial NoDerivatives)',
+              all_rights_reserved: 'All Rights Reserved'
+  }
+end
