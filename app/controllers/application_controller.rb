02085db78d9f97ad119f0bf9296ac6bfd95cc2b0
Bracken Mosbacker <bracken@lumenlearning.com>
2016-10-28 15:07:43 -0700

    allow app to load in an iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :allow_iframe
+
+  def allow_iframe
+    response.headers.delete "X-Frame-Options"
+  end
 end
