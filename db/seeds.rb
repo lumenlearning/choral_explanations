4c4b51f7f72d72b3589a4b093519ec9726697d4c
Bracken Mosbacker <bracken@lumenlearning.com>
2016-10-31 12:25:38 -0700

    add solute potential seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 context = Context.create!(name: 'Default', is_public: true)
 user = User.create!(name: "Linda Heilig")
 user2 = User.create!(name: "Mike C")
+user3 = User.create!(name: "Marguerite Wagner")
+user4 = User.create!(name: "Cleveland Dozier")
 
 # https://www.quora.com/What-is-mitosis-Why-is-it-important
 chorus = Chorus.create!(name: "What is mitosis? Why is it important?", user: user2, context: context, license: 'cc_by', description: "Help others understand the importance of Mitosis.")
@@ -34,3 +36,15 @@ HTML
 Response.create!(name: "Re: What is mitosis? Why is it important?", user: user2, context: context, chorus: chorus, license: 'cc_by', description: 'Response 2')
 Response.create!(name: "Re: What is mitosis? Why is it important?", context: context, chorus: chorus, license: 'cc_by', description: 'Response 3')
 Response.create!(name: "Re: What is mitosis? Why is it important?", context: context, chorus: chorus, license: 'cc_by', description: 'Response 4')
+
+
+
+chorus = Chorus.create!(name: "What is solute potential?", user: user3, context: context, license: 'cc_by', description: "Help others understand solute potential.")
+Response.create!(name: "It’s Why Slugs Melt!", user: user4, context: context, chorus: chorus, license: 'cc_by', description: <<HTML)
+<p>Maybe as a kid you were horrible (many kids are) and you tortured slugs in the back yard by putting salt on them and watching them shrivel up. Or maybe as a kid at the lake you were latched onto by leeches that were horrible (most leeches are) and you watched with amazement as you mom slayed them effortlessly with the salt shaker.</p>
+<p>If you think about what’s happening here, you’ll have a bit more of a grip on solute potential.</p>
+<p>Consider this. You put salt on the slug’s back, which mixes with the water on it. Now you have a membrane (slug skin) with very salty water on the outside and very non-salty water on the inside. The nature of things is in this case that osmosis wants to equalize the saltiness. So a bunch of the water inside the slug rushes past the membrane (skin) to the outside of the slug. Unfortunately, more water on the outside just dissolves more of the salt, which makes the salt imbalance worse, which in turn creates more osmotic pressure to push more water out of the slug. This cycle continues until the slug is a shriveled mess lying in a pool of salty water.</p>
+<p>So what’s the solute potential in this example? In this case salt is the solute and water is the solvent. So the solute potential here, at the point the salt is applied, is high on the inside of the slug and low on the outside of it. Putting the salt on the outside of the slug lowers the solute potential of the water on the outside of the slug.</p>
+<p>A way of remembering that is that the water on the inside of the slug has a “high potential” of moving to the outside of the slug. The osmotic pressure is a result of the difference in the potential on the two sides of that slug-skin membrane, the salty low-potential outside and the less salty high potential inside. The difference between the solute potential of the outside and the inside is one of a few factors that determine overall water potential.</p>
+<p>If you think about this, this is not just a property of slugs. People dry flowers with salt, and before refrigeration we would sometimes dehydrate foods in this way. That salty french fry on the floor of your car that lasts for years without molding? It’s been dehydrated too, and the way the water got from the inside to the outside of the fry is through a difference in solute potential.</p>
+HTML
